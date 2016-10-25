b1bc559a969e8fd30185613c335f7628e716b823
Max Wen <max.wen@qq.com>
2016-10-25 15:53:17 +0800

    namespaces and README.MD

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
-# yii2-EasyWeChat
-WeChat SDK for yii2 , based on overtrue/wechat.
+# yii2-easyWeChat
+WeChat SDK for yii2 , based on [overtrue/wechat](https://github.com/overtrue/wechat).  This extension helps you access `overtrue/wechat` application in a simple & familiar way.   
+
+## Installation
+```
+composer require maxwen/yii2-easyWeChat
+```
+
+## Configuration
+
+Add the SDK as a yii2 application `component` in the `config/main.php`:
+
+```php
+
+'components' => [
+	// ...
+	'wechat' => [
+		'class' => 'common\components\Wechat',
+		// 'userOptions' => []  # user identity class params
+		// 'sessionParam' => '' # wechat user info will be stored in session under this key
+		// 'returnUrlParam' => '' # returnUrl param stored in session
+	],
+	// ...
+]
+```
+
+## Usage
+```php
+
+// 微信网页授权:
+if(Yii::$app->wechat->isWechat && !Yii::$app->wechat->isAuthorized()) {
+	return Yii::$app->wechat->authorizeRequired()->send();
+}
+
+// 微信支付(JsApi):
+$orderData = [ 
+	'openid' => '.. '
+	// ... etc. 
+];
+$order = new WechatOrder($orderData);
+$payment = Yii::$app->wechat->payment;
+$prepayRequest = $payment->prepare($order);
+if($prepayRequest->return_code = 'SUCCESS' && $prepayRequest->result_code == 'SUCCESS') {
+	$prepayId = $prepayRequest->prepay_id;
+}else{
+	throw new yii\base\ErrorException('微信支付异常, 请稍后再试');
+}
+
+$jsApiConfig = $payment->configForPayment($prepayId);
+
+return $this->render('wxpay', [
+	'jsApiConfig' => $jsApiConfig,
+	'orderData'   => $orderData
+]);
+
+```
+
+
+### How to load Wechat configures?
+the `overtrue/wechat` application always constructs with a `$options` parameter. 
+I made the options as a yii2 param in the `params.php`:
+
+recomended way:
+```php
+// in this way you need to create a wechat.php in the same directory of params.php
+// put contents in the wechat.php like:
+// return [ 
+// 		// wechat options here 
+// ];
+'WECHAT' => require(__DIR__.'/wechat.php'),
+```
+OR 
+```php
+'WECHAT' => [ // wechat options here ]
+```
+
+[Wechat options configure help docs.](https://easywechat.org/zh-cn/docs/configuration.html)
+
+
+### More documentation
+see [EasyWeChat Docs](https://easywechat.org/zh-cn/docs/index.html).
+
+Thanks to `overtrue/wechat` , realy a easy way to play with wechat SDK 😁.
---
+++ Wechat.php
@@ -0,0 +1,167 @@
+<?php
+/**
+ * Project: yii2-easyWeChat.
+ * Author: Max.wen
+ * Date: <2016/05/10 - 14:31>
+ */
+
+namespace maxwen\easywechat;
+
+use yii;
+use EasyWeChat\Foundation\Application;
+use yii\base\Component;
+
+/**
+ * Class Wechat
+ * @package common\components
+ *
+ * @property Application $app
+ * @property WechatUser  $user
+ * @property bool        $isWechat
+ * @property string      $returnUrl
+ */
+class Wechat extends Component
+{
+	/**
+	 * user identity class params
+	 * @var array
+	 */
+	public $userOptions = [];
+	/**
+	 * wechat user info will be stored in session under this key
+	 * @var string
+	 */
+	public $sessionParam = '_wechatUser';
+	/**
+	 * returnUrl param stored in session
+	 * @var string
+	 */
+	public $returnUrlParam = '_wechatReturnUrl';
+	/**
+	 * @var Application
+	 */
+	private static $_app;
+	/**
+	 * @var WechatUser
+	 */
+	private static $_user;
+
+	/**
+	 * @return yii\web\Response
+	 */
+	public function authorizeRequired()
+	{
+		if(Yii::$app->request->get('code')) {
+			// callback and authorize
+			return $this->authorize($this->app->oauth->user());
+		}else{
+			// redirect to wechat authorize page
+			$this->setReturnUrl(Yii::$app->request->getUrl());
+			return Yii::$app->response->redirect($this->app->oauth->redirect()->getTargetUrl());
+		}
+	}
+	
+	/**
+	 * @param \Overtrue\Socialite\User $user
+	 * @return yii\web\Response
+	 */
+	public function authorize(\Overtrue\Socialite\User $user)
+	{
+		Yii::$app->session->set($this->sessionParam, $user->toJSON());
+		return Yii::$app->response->redirect($this->getReturnUrl());
+	}
+
+	/**
+	 * check if current user authorized
+	 * @return bool
+	 */
+	public function isAuthorized()
+	{
+		$hasSession = Yii::$app->session->has($this->sessionParam);
+		$sessionVal = Yii::$app->session->get($this->sessionParam);
+		return ($hasSession && !empty($sessionVal));
+	}
+
+	/**
+	 * @param string|array $url
+	 */
+	public function setReturnUrl($url)
+	{
+		Yii::$app->session->set($this->returnUrlParam, $url);
+	}
+
+	/**
+	 * @param null $defaultUrl
+	 * @return mixed|null|string
+	 */
+	public function getReturnUrl($defaultUrl = null)
+	{
+		$url = Yii::$app->getSession()->get($this->returnUrlParam, $defaultUrl);
+		if (is_array($url)) {
+			if (isset($url[0])) {
+				return Yii::$app->getUrlManager()->createUrl($url);
+			} else {
+				$url = null;
+			}
+		}
+
+		return $url === null ? Yii::$app->getHomeUrl() : $url;
+	}
+
+	/**
+	 * single instance of EasyWeChat\Foundation\Application
+	 * @return Application
+	 */
+	public function getApp()
+	{
+		if(! self::$_app instanceof Application){
+			self::$_app = new Application(Yii::$app->params['WECHAT']);
+		}
+		return self::$_app;
+	}
+
+	/**
+	 * @return WechatUser|null
+	 */
+	public function getUser()
+	{
+		if(!$this->isAuthorized()) {
+			return new WechatUser();
+		}
+
+		if(! self::$_user instanceof WechatUser) {
+			$userInfo = Yii::$app->session->get($this->sessionParam);
+			$config = $userInfo ? json_decode($userInfo, true) : [];
+			self::$_user = new WechatUser($config);
+		}
+		return self::$_user;
+	}
+
+	/**
+	 * overwrite the getter in order to be compatible with this component
+	 * @param $name
+	 * @return mixed
+	 * @throws \Exception
+	 */
+	public function __get($name)
+	{
+		try {
+			return parent::__get($name);
+		}catch (\Exception $e) {
+			if($this->app->$name) {
+				return $this->app->$name;
+			}else{
+				throw $e->getPrevious();
+			}
+		}
+	}
+
+	/**
+	 * check if client is wechat
+	 * @return bool
+	 */
+	public function getIsWechat()
+	{
+		return strpos($_SERVER["HTTP_USER_AGENT"], "MicroMessenger") !== false;
+	}
+}
