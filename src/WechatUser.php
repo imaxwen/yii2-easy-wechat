1e7620083fd3993f4e2636caf6ddd4b48f3f74fa
Max Wen <max.wen@qq.com>
2017-03-27 14:42:52 +0800

    move source code files to `src` folder

---
+++ src/WechatUser.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Project: yii2-easyWeChat.
+ * Author: Max.wen
+ * Date: <2016/05/10 - 17:17>
+ */
+
+namespace maxwen\easywechat;
+
+
+use yii\base\Component;
+
+/**
+ * Class WechatUser
+ * @package common\components
+ *
+ * @property string $openId
+ */
+class WechatUser extends Component
+{
+	/**
+	 * @var string
+	 */
+	public $id;
+	/**
+	 * @var string
+	 */
+	public $nickname;
+	/**
+	 * @var string
+	 */
+	public $name;
+	/**
+	 * @var string
+	 */
+	public $email;
+	/**
+	 * @var string
+	 */
+	public $avatar;
+	/**
+	 * @var array
+	 */
+	public $original;
+	/**
+	 * @var \Overtrue\Socialite\AccessToken
+	 */
+	public $token;
+
+	/**
+	 * @return string
+	 */
+	public function getOpenId()
+	{
+		return isset($this->original['openid']) ? $this->original['openid'] : '';
+	}
+}
